6a66dbca6acbad708799a8761f39941773a5b253
valeriauribe2 <valeria_uribe@hotmail.com>
2024-08-19 10:07:49 -0500

    commit

--- WebUI/src/App.js
+++ WebUI/src/App.js
@@ -1,59 +1,32 @@
-import "./App.css";
-import Confetti from "./Confetti";
+import React, { useState, useEffect } from 'react';
 
-const shareMessage = "I just ran my first container using Docker";
-const shareLink = "https://docker.com/";
+function App() {
+  const [data, setData] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
 
-const App = () => {
+  useEffect(() => {
+    fetch('http://localhost:8080/')
+      .then(response => response.json())
+      .then(data => {
+        setData(data.message); // Assuming your API returns an object with a message property
+        setLoading(false);
+      })
+      .catch(err => {
+        setError('Failed to fetch data from Ollama');
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
   return (
-    <div className="App">
-      <Confetti />
-      <header className="App-header">
-        <h1 style={{ marginBottom: "0px" }}>Congratulations!!!</h1>
-        <p style={{ marginTop: "10px", marginBottom: "50px" }}>
-          You ran your first container.
-        </p>
-        <div>
-          <a
-            target="_blank"
-            href={
-              "https://twitter.com/intent/tweet?text=" +
-              shareMessage +
-              "&url=" +
-              shareLink
-            }
-            class="fa-brands fa-x-twitter"
-            rel="noopener noreferrer"
-          >
-            {" "}
-          </a>
-          <a
-            target="_blank"
-            href={
-              "https://www.linkedin.com/sharing/share-offsite/?url=" + shareLink
-            }
-            class="fa-brands fa-linkedin"
-            rel="noopener noreferrer"
-          >
-            {" "}
-          </a>
-          <a
-            target="_blank"
-            href={
-              "https://reddit.com/submit?title=" +
-              shareMessage +
-              "&url=" +
-              shareLink
-            }
-            class="fa-brands fa-reddit"
-            rel="noopener noreferrer"
-          >
-            {" "}
-          </a>
-        </div>
-      </header>
+    <div>
+      <h1>Welcome to WebUI</h1>
+      <p>Data from Ollama: {data}</p>
     </div>
   );
-};
+}
 
 export default App;
