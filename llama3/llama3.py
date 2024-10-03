a82507e1ec04cda504084afefbfaab444832b179
Valeria Uribe <valeriauribe@valerias-mbp-2.mynetworksettings.com>
2024-10-03 17:00:50 -0500

    iamges

--- llama3/llama3.py
+++ llama3/llama3.py
@@ -3,8 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Llama3!'
+def home():
+    return "Llama3 is running!"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
+    
